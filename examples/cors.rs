8217b74c83a6086acea2521e4dc559da5bf429e1
James <james@prestwi.ch>
2025-05-19 09:57:44 -0400

    fix: better corsing

--- examples/cors.rs
+++ examples/cors.rs
@@ -1,22 +1,36 @@
 use axum::http::{HeaderValue, Method};
+use eyre::{ensure, Context};
 use std::{future::IntoFuture, net::SocketAddr};
 use tower_http::cors::{AllowOrigin, Any, CorsLayer};
 
-fn make_cors(cors: Option<&str>) -> tower_http::cors::CorsLayer {
-    let cors = cors
-        .unwrap_or("*")
-        .parse::<HeaderValue>()
-        .map(Into::<AllowOrigin>::into)
-        .unwrap_or_else(|_| AllowOrigin::any());
+fn make_cors(cors: Option<&str>) -> eyre::Result<CorsLayer> {
+    let origins = match cors {
+        None | Some("*") => AllowOrigin::any(),
+        Some(cors) => {
+            ensure!(
+                !cors.split(',').any(|o| o == "*"),
+                "Wildcard '*' is not allowed in CORS domains"
+            );
 
-    CorsLayer::new()
+            cors.split(',')
+                .map(|domain| {
+                    domain
+                        .parse::<HeaderValue>()
+                        .wrap_err_with(|| format!("Invalid CORS domain: {}", domain))
+                })
+                .collect::<Result<Vec<_>, _>>()?
+                .into()
+        }
+    };
+
+    Ok(CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
-        .allow_origin(cors)
-        .allow_headers(Any)
+        .allow_origin(origins)
+        .allow_headers(Any))
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> eyre::Result<()> {
     let cors = std::env::args().nth(1);
     let port = std::env::args()
         .nth(2)
@@ -33,7 +47,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Ok::<_, ()>(a + b)
         })
         .into_axum("/")
-        .layer(make_cors(cors.as_deref()));
+        .layer(make_cors(cors.as_deref())?);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let listener = tokio::net::TcpListener::bind(addr).await?;
